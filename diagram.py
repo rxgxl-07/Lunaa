08d3bbb0d9e3d80c9ebe1a65192cfec779a19592
rxgxl-07 <ragulecesckt@gmail.com>
2026-08-01 20:20:33 +0530

    updated files with current

--- diagram.py
+++
@@ -1,8 +0,0 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
-
-
-with Diagram("AWS Architecture", filename="image", outformat="png", show=False):
-    EC2("Web Server") >> RDS("Database")>> ELB("Storage")
--- lab.java
+++
@@ -1,5 +0,0 @@
-public class Lab {
-    public static void main(String[] args) {
-        System.out.println("Experiment 3");
-    }
-}
